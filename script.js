490df90702c3ec7befc98567e0cf1fa9b0d80ba6
Lyndon <lyndon.stluce@gmail.com>
2022-03-15 16:53:39 -0400

    fly spirte added

--- script.js
+++ script.js
@@ -3,33 +3,56 @@ const gameField = document.getElementById("gameField");
 const gameCtx = gameField.getContext("2d");
 GAMEFIELD_WIDTH = gameField.width = 600;
 GAMEFIELD_HEIGHT = gameField.height = 500;
+const numberOfFlies = 15;
+const flyArray = [];
+
+const flyImage = new Image();
+flyImage.src = 'fly-sprite.gif';
+
+
 
 let header = document.getElementById("header")
 header.textContent += "kill the flies..."
 
 
+
 class Enemy {
   constructor() {
-    this.x = 10;
-    this.y = 50;
-    this.width = 50;
-    this.height = 50;
+
+
+    //this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 500;
+    this.spriteHeight = 380;
+    this.width = this.spriteWidth / 6;
+    this.height = this.spriteHeight / 6;
+    this.x = Math.random() * (gameField.width);
+    this.y = Math.random() * (gameField.height);
+    this.frame = 0;
   }
   update() {
-    this.x++;
-    this.y++;
+    this.x += Math.random() * 10 - 5.5;
+    this.y += Math.random() * 24 - 10.5;
+
+    //this.frame > 0 ? this.frame = 0 : this.frame++;
   }
   draw() {
-    gameCtx.fillRect(this.x, this.y, this.width, this.height);
+    gameCtx.strokeRect(this.x, this.y, this.width, this.height);
+    gameCtx.drawImage(flyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
   }
 };
-const flyObject = new Enemy();
+
+for (let i = 0; i < numberOfFlies; i++) {
+  flyArray.push(new Enemy());
+};
 
 
 function flyAnimate() {
   gameCtx.clearRect(0, 0, GAMEFIELD_WIDTH, GAMEFIELD_HEIGHT);
-  flyObject.update();
-  flyObject.draw();
+
+  flyArray.forEach(enemy => {
+    enemy.update();
+    enemy.draw();
+  })
   requestAnimationFrame(flyAnimate)
 }
 flyAnimate()
