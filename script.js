4d6f268b6a4b164c3b88cfe5822605d8d6796e12
Lyndon <lyndon.stluce@gmail.com>
2022-03-15 11:55:57 -0400

    made moving object

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
+/**@type {HTMLDivElement} */
+const gameField = document.getElementById("gameField");
+const gameCtx = gameField.getContext("2d");
+GAMEFIELD_WIDTH = gameField.width = 600;
+GAMEFIELD_HEIGHT = gameField.height = 500;
+
 let header = document.getElementById("header")
 header.textContent += "kill the flies..."
+
+
+class Enemy {
+  constructor() {
+    this.x = 10;
+    this.y = 50;
+    this.width = 50;
+    this.height = 50;
+  }
+  update() {
+    this.x++;
+    this.y++;
+  }
+  draw() {
+    gameCtx.fillRect(this.x, this.y, this.width, this.height);
+  }
+};
+const flyObject = new Enemy();
+
+
+function flyAnimate() {
+  gameCtx.clearRect(0, 0, GAMEFIELD_WIDTH, GAMEFIELD_HEIGHT);
+  flyObject.update();
+  flyObject.draw();
+  requestAnimationFrame(flyAnimate)
+}
+flyAnimate()
