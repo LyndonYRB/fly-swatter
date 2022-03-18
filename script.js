48dd78e13a08112cf7d5ea122e58e0bf673ffa73
Lyndon <lyndon.stluce@gmail.com>
2022-03-18 11:52:04 -0400

    with a tutorial i found that uses a canvas element

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 window.onload = function () {
+  //playing field
   const body = document.body;
   field = document.getElementById("gameField");
   console.log(body);
   GAMEFIELD_WIDTH = gameField.width = 600;
   GAMEFIELD_HEIGHT = gameField.height = 500;
+  console.log(body);
+  console.log(gameField.width)
+  console.log(gameField.height)
 
-
+  // the flies
 
   createDuck = () => {
     let duckChara = document.createElement('div');
-    duckChara.classList.add('duck');
+    duckChara.classList.add('fly');
     document.getElementById("gameField").appendChild(duckChara);
 
 
