937f4950acc84b875a50964c9c6c3e4f9571a958
Lyndon <lyndon.stluce@gmail.com>
2022-03-21 03:37:11 -0400

    got the fly to move aroun

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
 let gameField = document.getElementById("gameField");
 
-moveFly = () => {
-  createFly.x += Math.random() * 5;
-  createFly.x += Math.random() * 5;
-}
+
+
+let cake = document.createElement("div");
+cake.classList.add("cake")
+gameField.appendChild(cake)
+
+
 
 createFly = () => {
   let flyDiv = document.createElement("div");
@@ -16,15 +20,34 @@ createFly = () => {
   flyDiv.style.top = Math.floor(Math.random() * 90 + 5) + '%';
   flyDiv.style.left = Math.floor(Math.random() * 90 + 5) + '%';
 
+  let deltaX = 0;
+  let deltaY = 0;
 
 
+
+  function animationframe() {
+    if (Math.random() > 0.95) {
+      deltaX = Math.floor(Math.random() * 10);
+      deltaY = Math.floor(Math.random() * 10);
+
+      if (Math.random() < 0.5) {
+        deltaX *= -1;
+      }
+
+      if (Math.random() < 0.5) {
+        deltaY *= -1;
+      }
+    }
+    flyDiv.style.top = '' + Math.min(Math.max(parseInt(flyDiv.style.top) + deltaY, 0), window.innerHeight - 170) + 'px';
+    flyDiv.style.left = '' + Math.min(Math.max(parseInt(flyDiv.style.left) + deltaX, 0), window.innerWidth - 110) + 'px';
+
+    window.requestAnimationFrame(animationframe);
+
+
+  }
+
+  window.requestAnimationFrame(animationframe);
 }
-animate = () => {
-  setTimeout(animate, 200);
-  moveFly()
-}
+
 createFly()
-
-animate()
-cake = document.createElement("div");
 
